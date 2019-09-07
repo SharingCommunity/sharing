65cd8e462e1e2098473eb49d1587f357f4711d2c
LeanKhan <eslean20@gmail.com>
2019-09-07 20:59:40 +0100

    feat: add tabs to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,45 @@
 <template>
   <div id="sidebar" class="my-2 shadow-sm">
-    <b-alert show variant="secondary">Your posts show up here</b-alert>
-    <div class="no-posts">
-      <p>🤷</p>
-      <p>You don't have any posts</p>
+    <!-- <b-alert show variant="secondary">{{
+      this.$store.getters.USER.Username
+    }}</b-alert> -->
+    <div class="h100">
+      <b-tabs fill pills>
+        <b-tab v-b-tooltip.hpver title="Posts" class="text-bold" active>
+          <template v-slot:title>
+            <!-- Uncomment for emoji label -->
+            <!-- &#127880; &nbsp; -->
+            Posts
+          </template>
+
+          <div class="no-posts">
+            <p>🤷</p>
+            <p>You don't have any posts</p>
+          </div>
+        </b-tab>
+        <b-tab v-b-tooltip.hover title="Chats" class="text-bold">
+          <template v-slot:title>
+            <!-- Uncomment for emoji label -->
+            <!-- &#128172; &nbsp; -->
+            Chats
+          </template>
+          <div class="no-posts">
+            <p>🤷</p>
+            <p>You don't have any chats</p>
+          </div>
+        </b-tab>
+        <b-tab v-b-tooltip.hover title="Activity" class="text-bold">
+          <template v-slot:title>
+            <!-- Uncomment for emoji label -->
+            <!-- &#9889; &nbsp; -->
+            Activity
+          </template>
+          <div class="no-posts">
+            <p>🤷</p>
+            <p>Nothing here</p>
+          </div>
+        </b-tab>
+      </b-tabs>
     </div>
   </div>
 </template>
@@ -14,7 +50,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 #sidebar {
   background-color: #d1ecf1;
   border: 2px solid #bee5eb;
@@ -27,5 +63,10 @@ export default {
 }
 .no-posts {
   font-size: 30px;
+}
+ul.nav-pills {
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.08) !important;
+  background-color: #f9f9f9 !important;
+  padding: 0.5rem 0.25rem;
 }
 </style>
