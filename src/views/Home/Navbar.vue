f2d0d78ba57943156e92dbc9615410593301d779
LeanKhan <eslean20@gmail.com>
2019-10-04 14:23:18 +0100

    feat: create navbar

---
+++ src/views/Home/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-navbar
+    class="text-white shadow-sm bg-solid py-1 justify-content-between"
+    fixed="top"
+  >
+    <b-navbar-brand>
+      Sharing
+    </b-navbar-brand>
+    <div>
+      <div class="notifs close">
+        <eva-icon
+          name="bell-outline"
+          fill="white"
+          width="25px"
+          height="25px"
+        ></eva-icon>
+      </div>
+    </div>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+<style scoped>
+.navbar {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif !important;
+}
+.bg-solid {
+  /* background-color: white; */
+  background-color: #20c997;
+}
+.notifs {
+  cursor: pointer;
+}
+</style>
