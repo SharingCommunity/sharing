50666eeb063579c92bbeac4a49178119f7d3da49
LeanKhan <eslean20@gmail.com>
2020-01-24 11:58:10 +0100

    chore: change webpack bundle analyzer settings

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 module.exports = {
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()]
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: "disabled",
+        generateStatsFile: true
+      })
+    ]
   }
 };
